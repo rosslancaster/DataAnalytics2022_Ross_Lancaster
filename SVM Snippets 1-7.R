53f63461ada88a0346ca8eb97f47f531d1ae98d7
rosslancaster <98289688+rosslancaster@users.noreply.github.com>
2022-04-07 10:54:02 -0400

    Add files via upload

---
+++ SVM Snippets 1-7.R
@@ -0,0 +1,230 @@
+#SVM 1
+
+n <- 150 # number of data points
+p <- 2 # dimension
+sigma <- 1 # variance of the distribution
+meanpos <- 0 # centre of the distribution of positive examples
+meanneg <- 3 # centre of the distribution of negative examples
+npos <- round(n/2) # number of positive examples
+nneg <- n-npos # number of negative examples
+# Generate the positive and negative examples
+xpos <- matrix(rnorm(npos*p,mean=meanpos,sd=sigma),npos,p)
+xneg <- matrix(rnorm(nneg*p,mean=meanneg,sd=sigma),npos,p)
+x <- rbind(xpos,xneg)
+# Generate the labels
+y <- matrix(c(rep(1,npos),rep(-1,nneg)))
+# Visualize the data
+plot(x,col=ifelse(y>0,1,2))
+legend("topleft",c('Positive','Negative'),col=seq(2),pch=1,text.col=seq(2))
+#
+ntrain <- round(n*0.8) # number of training examples
+tindex <- sample(n,ntrain) # indices of training samples
+xtrain <- x[tindex,]
+xtest <- x[-tindex,]
+ytrain <- y[tindex]
+ytest <- y[-tindex]
+istrain=rep(0,n)
+istrain[tindex]=1
+# Visualize
+plot(x,col=ifelse(y>0,1,2),pch=ifelse(istrain==1,1,2))
+legend("topleft",c('Positive Train','Positive Test','Negative Train','Negative Test'),col=c(1,1,2,2), pch=c(1,2,1,2), text.col=c(1,1,2,2))
+
+#Overall, data appears to mostly be linearly separable
+#--------------------------------------------------------------------
+#SVM 2
+
+library(e1071)
+library(rpart)
+install.packages("mlbench")
+library(mlbench)
+## split data into a train and test set
+data(Ozone,package='mlbench')
+index <- 1:nrow(Ozone)
+testindex <- sample(index, trunc(length(index)/3))
+testset <- na.omit(Ozone[testindex,-3])
+trainset <- na.omit(Ozone[-testindex,-3])
+svm.model <- svm(V4 ~ ., data = trainset, cost = 1000, gamma = 0.0001)
+svm.pred <- predict(svm.model, testset[,-3])
+crossprod(svm.pred - testset[,3]) / length(testindex)
+#--------------------------------------------------------------------
+#SVM 3
+
+data(iris)
+attach(iris)
+
+## classification mode
+# default with factor response:
+model <- svm(Species ~ ., data = iris)
+
+# alternatively the traditional interface:
+x <- subset(iris, select = -Species)
+y <- Species
+model <- svm(x, y) 
+
+print(model)
+summary(model)
+
+# test with train data
+pred <- predict(model, x)
+# (same as:)
+pred <- fitted(model)
+
+# Check accuracy:
+table(pred, y)
+
+# compute decision values and probabilities:
+pred <- predict(model, x, decision.values = TRUE)
+attr(pred, "decision.values")[1:4,]
+
+# visualize (classes by color, SV by crosses):
+plot(cmdscale(dist(iris[,-5])),
+     col = as.integer(iris[,5]),
+     pch = c("o","+")[1:150 %in% model$index + 1])
+
+## try regression mode on two dimensions
+
+# create data
+x <- seq(0.1, 5, by = 0.05)
+y <- log(x) + rnorm(x, sd = 0.2)
+
+# estimate model and predict input values
+m   <- svm(x, y)
+new <- predict(m, x)
+
+# visualize
+plot(x, y)
+points(x, log(x), col = 2)
+points(x, new, col = 4)
+
+## density-estimation
+
+# create 2-dim. normal with rho=0:
+X <- data.frame(a = rnorm(1000), b = rnorm(1000))
+attach(X)
+
+# traditional way:
+m <- svm(X, gamma = 0.1)
+
+# formula interface:
+m <- svm(~., data = X, gamma = 0.1)
+# or:
+m <- svm(~ a + b, gamma = 0.1)
+
+# test:
+newdata <- data.frame(a = c(0, 4), b = c(0, 4))
+predict (m, newdata)
+
+# visualize:
+plot(X, col = 1:1000 %in% m$index + 1, xlim = c(-5,5), ylim=c(-5,5))
+points(newdata, pch = "+", col = 2, cex = 5)
+
+# weights: (example not particularly sensible)
+i2 <- iris
+levels(i2$Species)[3] <- "versicolor"
+summary(i2$Species)
+wts <- 100 / table(i2$Species)
+wts
+m <- svm(Species ~ ., data = i2, class.weights = wts)
+
+#The model was very good at predicting virginica, versicoor, and setosa
+#----------------------------------------------------------------
+#SVm 4
+
+promotergene= read.csv(url("https://archive.ics.uci.edu/ml/machine-learning-databases/molecular-biology/promoter-gene-sequences/"), header=FALSE)
+
+## example using the promotergene data set
+data(promotergene)
+
+## create test and training set
+ind <- sample(1:dim(promotergene)[1],20)
+genetrain <- promotergene[-ind, ]
+genetest <- promotergene[ind, ]
+
+## train a support vector machine
+gene = ksvm(Class~.,data=genetrain,kernel="rbfdot",
+              kpar=list(sigma=0.015),C=70,cross=4,prob.model=TRUE)
+
+## predict gene type probabilities on the test set
+genetype <- predict(gene,genetest,type="probabilities")
+
+#I had difficulty loading the dataset for this snippet
+#-----------------------------------------------------------------
+#SVM 5
+
+library(e1071) 
+m1 <- matrix( c( 
+  0,    0,    0,    1,    1,    2,     1, 2,    3,    2,    3, 3, 0, 1,2,3, 
+  0, 1, 2, 3, 
+  1,    2,    3,    2,    3,    3,     0, 0,    0,    1, 1, 2, 4, 4,4,4,    0, 
+  1, 2, 3, 
+  1,    1,    1,    1,    1,    1,    -1,-1,  -1,-1,-1,-1, 1 ,1,1,1,     1, 
+  1,-1,-1 
+), ncol = 3 ) 
+
+Y = m1[,3] 
+X = m1[,1:2] 
+
+df = data.frame( X , Y ) 
+
+par(mar=c(1,1,1,1)) 
+for( cost in c( 1e-3 ,1e-2 ,1e-1, 1e0,  1e+1, 1e+2 ,1e+3)) { 
+cost <- 1 
+model.svm <- svm( Y ~ . , data = df ,  type = "C-classification" , kernel = "linear", cost = cost, scale =FALSE ) 
+print(model.svm$SV) 
+  
+plot(x=0,ylim=c(0,5), xlim=c(0,3),main= paste( "cost: ",cost, "#SV: ",nrow(model.svm$SV))) 
+points(m1[m1[,3]>0,1], m1[m1[,3]>0,2], pch=3, col="green") 
+points(m1[m1[,3]<0,1], m1[m1[,3]<0,2], pch=4, col="blue") 
+points(model.svm$SV[,1],model.svm$SV[,2], pch=18 , col = "red") }
+
+#had to change the file margins, took a bit to figure out
+#---------------------------------------------------------------
+#SVM 6
+install.packages("kernlab")
+library(kernlab)
+data(spam)
+
+## create test and training set
+index <- sample(1:dim(spam)[1])
+spamtrain <- spam[index[1:floor(dim(spam)[1]/2)], ]
+spamtest <- spam[index[((ceiling(dim(spam)[1]/2)) + 1):dim(spam)[1]], ]
+
+## train a support vector machine
+filter <- ksvm(type~.,data=spamtrain,kernel="rbfdot",
+               kpar=list(sigma=0.05),C=5,cross=3)
+filter
+
+## predict mail type on the test set
+mailtype <- predict(filter,spamtest[,-58])
+
+## Check results
+table(mailtype,spamtest[,58])
+
+#Was okay at okay predicting spam versus not spam, it is not amazing though
+#Could have been better
+#--------------------------------------------------------------------------
+#SVM 7
+
+## Another example with the famous iris data
+data(iris)
+
+## Create a kernel function using the build in rbfdot function
+rbf <- rbfdot(sigma=0.1)
+rbf
+
+## train a bound constraint support vector machine
+irismodel <- ksvm(Species~.,data=iris,type="C-bsvc",
+                  kernel=rbf,C=10,prob.model=TRUE)
+
+irismodel
+
+## get fitted values
+fitted(irismodel)
+
+## Test on the training set with probabilities as output
+predict(irismodel, iris[,-5], type="probabilities")
+
+#for the most part probabilities seem to be high and accurate
+
+
+
